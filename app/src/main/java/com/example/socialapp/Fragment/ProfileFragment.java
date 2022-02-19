2987c45095dc61774d0b2f2164e3b000734f712e
Rahul <rketu786@gmail.com>
2022-02-20 01:42:29 +0530

    ProfileFragment Complete (without Toolbar)

--- app/src/main/java/com/example/socialapp/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/socialapp/Fragment/ProfileFragment.java
@@ -3,15 +3,23 @@ package com.example.socialapp.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.socialapp.Adapter.FriendAdapter;
+import com.example.socialapp.Model.FriendModel;
 import com.example.socialapp.R;
+import com.example.socialapp.databinding.FragmentPostBinding;
+import com.example.socialapp.databinding.FragmentProfileBinding;
+
+import java.util.ArrayList;
 
 public class ProfileFragment extends Fragment {
-
+    FragmentProfileBinding binding;
+    ArrayList<FriendModel> friendList;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +30,22 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+
+        // Arraylist for RecyclerView
+        friendList = new ArrayList<>();
+        friendList.add(new FriendModel(R.drawable.image_face));
+        friendList.add(new FriendModel(R.drawable.image_wall));
+        friendList.add(new FriendModel(R.drawable.back_ground));
+
+
+        // Setting Adapter to RecyclerView
+        FriendAdapter friendAdapter = new FriendAdapter(friendList, getContext());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
+        binding.friends.setLayoutManager(linearLayoutManager);
+
+        binding.friends.setAdapter(friendAdapter);
+
+        return binding.getRoot();
     }
 }
---
+++ app/src/main/java/com/example/socialapp/Model/FriendModel.java
@@ -0,0 +1,17 @@
+package com.example.socialapp.Model;
+
+public class FriendModel {
+    int friendImg;
+
+    public int getFriendImg() {
+        return friendImg;
+    }
+
+    public void setFriendImg(int friendImg) {
+        this.friendImg = friendImg;
+    }
+
+    public FriendModel(int friendImg) {
+        this.friendImg = friendImg;
+    }
+}
