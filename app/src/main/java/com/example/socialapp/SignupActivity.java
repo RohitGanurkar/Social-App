0e898de4de129d51d6e1e3e8ce6b1699ebf2d55e
Rahul <rketu786@gmail.com>
2022-02-23 01:11:48 +0530

    Firebase Added @ SignupActivity complete

--- app/src/main/java/com/example/socialapp/SignupActivity.java
+++ app/src/main/java/com/example/socialapp/SignupActivity.java
@@ -1,20 +1,76 @@
 package com.example.socialapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.socialapp.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class SignupActivity extends AppCompatActivity {
     ActivitySignupBinding binding;
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignupBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        // when SignUp button was clicked
+        binding.signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = Objects.requireNonNull(binding.emailText.getText()).toString();
+                final String password = Objects.requireNonNull(binding.passwordText.getText()).toString();
+
+                if (email.length() > 0 || password.length() > 0){
+                    // for Create User in Firebase
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        User user = new User(binding.nameText.getText().toString(),binding.professionText.getText().toString(), email, password);
+
+                                        // save UserInfo in Database
+                                        String userId = task.getResult().getUser().getUid();
+                                        database.getReference().child("User").child(userId).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+                                                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                                                startActivity(intent);
+                                            }
+                                        });
+
+                                        Toast.makeText(SignupActivity.this, "Successfully Created", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(SignupActivity.this, "Some Error Occurred", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+                else {
+                    Toast.makeText(SignupActivity.this, "Please fill all the Details", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         // for goto LogIn Page
         binding.gotoLogin.setOnClickListener(new View.OnClickListener() {
