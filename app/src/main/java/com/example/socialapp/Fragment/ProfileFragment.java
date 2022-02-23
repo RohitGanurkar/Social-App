b337a05e5bfb8207704ae15b7678c1f68d76b2af
Rahul <rketu786@gmail.com>
2022-02-23 16:08:57 +0530

    Login Complete & FirebaseStorage setup (store cover photo)

--- app/src/main/java/com/example/socialapp/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/socialapp/Fragment/ProfileFragment.java
@@ -1,29 +1,52 @@
 package com.example.socialapp.Fragment;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.socialapp.Adapter.FriendAdapter;
 import com.example.socialapp.Model.FriendModel;
 import com.example.socialapp.R;
 import com.example.socialapp.databinding.FragmentPostBinding;
 import com.example.socialapp.databinding.FragmentProfileBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 
 public class ProfileFragment extends Fragment {
     FragmentProfileBinding binding;
     ArrayList<FriendModel> friendList;
+    ActivityResultLauncher<String> mGetContent;
+    FirebaseAuth auth;
+    FirebaseStorage storage;
+    FirebaseDatabase database;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // get Instance of Firebase
+        auth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
+        database = FirebaseDatabase.getInstance();
     }
 
     @Override
@@ -38,13 +61,41 @@ public class ProfileFragment extends Fragment {
         friendList.add(new FriendModel(R.drawable.image_wall));
         friendList.add(new FriendModel(R.drawable.back_ground));
 
-
         // Setting Adapter to RecyclerView
         FriendAdapter friendAdapter = new FriendAdapter(friendList, getContext());
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
         binding.friends.setLayoutManager(linearLayoutManager);
-
         binding.friends.setAdapter(friendAdapter);
+
+        // when we click for ChangeCoverPhoto
+        binding.addCover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGetContent.launch("image/*");
+            }
+        });
+        /* StartActivityForResult :- {{ ALTERNATIVE }}
+         when user select the image by clicking */
+        mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                binding.coverPhoto.setImageURI(result);
+
+                StorageReference reference = storage.getReference().child("cover_photo").child(auth.getUid());
+                reference.putFile(result).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        Toast.makeText(getContext(), "Successfully Change", Toast.LENGTH_SHORT).show();
+                        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                database.getReference().child("User").child(auth.getUid()).child("coverPhoto").setValue(uri.toString());
+                            }
+                        });
+                    }
+                });
+            }
+        });
 
         return binding.getRoot();
     }
--- app/src/main/java/com/example/socialapp/LoginActivity.java
+++ app/src/main/java/com/example/socialapp/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.socialapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,14 +9,48 @@ import android.view.View;
 
 import com.example.socialapp.databinding.ActivityLoginBinding;
 import com.example.socialapp.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
     ActivityLoginBinding binding;
+    FirebaseAuth auth;
+    FirebaseUser currentUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // getting Instance of Firebase
+        auth = FirebaseAuth.getInstance();
+        currentUser = auth.getCurrentUser();
+
+        // when login Button was clicked user can login
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = Objects.requireNonNull(binding.emailText.getText()).toString();
+                final String password = Objects.requireNonNull(binding.passwordText.getText()).toString();
+
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            // when user log in move to Main Activity
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                });
+            }
+        });
 
         // for goto SighUp Page
         binding.gotoSignup.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +60,15 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // if user AllReady Login then goto MainActivity
+        if(currentUser != null){
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
